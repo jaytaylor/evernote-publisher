f4e09ea239258693872b81172c4da90cdeac8193
Jay Taylor <outtatime@gmail.com>
2016-07-25 09:28:22 -0700

    Note page updates:
        - Added google search link.
        - Centered top title header.
        - Added clip date.

--- lib/generator.py
+++ lib/generator.py
@@ -2,7 +2,7 @@
 
 """Note renderer."""
 
-import base64, datetime, simplejson as json, glob, os, settings, shutil, unicodedata
+import base64, datetime, simplejson as json, glob, os, settings, shutil, unicodedata, urllib
 from bs4 import BeautifulSoup
 from jinja2 import Template, DictLoader
 from jinja2.environment import Environment
@@ -85,6 +85,7 @@ class HtmlGenerator(object):
             with open(path, 'r') as fh:
                 data = json.load(fh)
                 data['title'] = BeautifulSoup(data['title'], 'html.parser', from_encoding='iso8859-15').string
+                data['urlencoded_title'] = urllib.quote_plus(data['title'].encode('utf-8'))
             listing.append({'id': jsonFileName[0:jsonFileName.index('.')], 'data': data})
 
         notes = sorted(filter(lambda n: n.deleted is not True, map(lambda d: Note(d), listing)), key=lambda n: n.createdTs, reverse=True)
